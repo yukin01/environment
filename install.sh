5a804406a060b79d1a05b3cf315516bb92a09d98
yukin01 <bump1105@gmail.com>
2019-07-05 13:43:34 +0900

    add vein.vim

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ set -eu
 
 # oh-my-zsh
 echo "===== install oh-my-zsh ====="
+sleep 0.5
 if [ -d ~/.oh-my-zsh ]; then
     echo "already installed"
 else
@@ -11,9 +12,24 @@ else
     cp ./cobalt2/cobalt2.zsh-theme ~/.oh-my-zsh/themes/
     echo "please change iTerm2 settings"
 fi
+echo ""
+
+# dein.vim
+# is needed?
+# https://github.com/Shougo/dein.vim
+# https://qiita.com/sugamondo/items/fcaf210ca86d65bcaca8
+echo "===== install dein.vim ====="
+sleep 0.5
+if [ -d ~/.cache/dein ]; then
+    echo "already installed"
+else
+    sh .vim/dein/installer.sh ~/.cache/dein
+fi
+echo ""
 
 # ruby
 echo "===== install ruby via rbenv ====="
+sleep 0.5
 if which ruby | grep .rbenv >/dev/null; then 
     echo "already installed"
 else
@@ -22,26 +38,29 @@ else
     rbenv global 2.5.1
     rbenv rehash
 fi
-echo "
-path: $(which ruby)
-version: $(ruby -v)
-"
+echo ""
+echo "path: $(which ruby)"
+echo "version: $(ruby -v)"
+echo ""
 
 # bundler
 echo "===== install bundler via gem ====="
+sleep 0.5
+# one liner sample
 # which bundle > /dev/null 2>&1 || gem install bundler
 if which bundle > /dev/null; then
     echo "already installed"
 else
     gem install bundler
 fi
-echo "
-path: $(which bundler)
-version: $(bundler -v)
-"
+echo ""
+echo "path: $(which bundler)"
+echo "version: $(bundler -v)"
+echo ""
 
 # node
 echo "===== install node via nodebrew ====="
+sleep 0.5
 if which node | grep .nodebrew >/dev/null; then 
     echo "already installed"
 else
@@ -49,17 +68,17 @@ else
     nodebrew install stable
     nodebrew use stable
 fi
-echo "
-node_path: $(which node)
-node_version: $(node -v)
-
-npm_path: $(which npm)
-npm_version: $(npm -v)
-"
-# echo "npm_path: $(which npm)\nnpm_version: $(npm -v)\n"
+echo ""
+echo "node_path: $(which node)"
+echo "node_version: $(node -v)"
+echo ""
+echo "npm_path: $(which npm)"
+echo "npm_version: $(npm -v)"
+echo ""
 
 # go
 echo "===== install go via goenv ====="
+sleep 0.5
 if which go | grep .goenv >/dev/null; then 
     echo "already installed"
 else
@@ -67,9 +86,9 @@ else
     goenv global 1.11.2
     goenv rehash
 fi
-echo "
-path: $(which go)
-version: $(go version)
-"
+echo ""
+echo "path: $(which go)"
+echo "version: $(go version)"
+echo ""
 
 echo "Installed successfully"
