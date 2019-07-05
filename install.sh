1a2ea4d99a0cc3bf24687be6b8c2271127e6c59e
yukin01 <bump1105@gmail.com>
2019-07-05 11:09:15 +0900

    update

--- install.sh
+++ install.sh
@@ -12,34 +12,64 @@ else
     echo "please change iTerm2 settings"
 fi
 
-set +e
-
 # ruby
 echo "===== install ruby via rbenv ====="
-eval "$(rbenv init -)"
-rbenv install 2.5.1
-rbenv global 2.5.1
-rbenv rehash
-echo "\npath: $(which ruby)\nversion: $(ruby -v)\n"
+if which ruby | grep .rbenv >/dev/null; then 
+    echo "already installed"
+else
+    eval "$(rbenv init -)"
+    rbenv install 2.5.1
+    rbenv global 2.5.1
+    rbenv rehash
+fi
+echo "
+path: $(which ruby)
+version: $(ruby -v)
+"
 
 # bundler
 echo "===== install bundler via gem ====="
-gem install bundler
-echo "\npath: $(which bundler)\nversion: $(bundler -v)\n"
+# which bundle > /dev/null 2>&1 || gem install bundler
+if which bundle > /dev/null; then
+    echo "already installed"
+else
+    gem install bundler
+fi
+echo "
+path: $(which bundler)
+version: $(bundler -v)
+"
 
 # node
 echo "===== install node via nodebrew ====="
-mkdir -p ~/.nodebrew/src
-nodebrew install stable
-nodebrew use stable
-echo "\nnode_path: $(which node)\nnode_version: $(node -v)"
-echo "npm_path: $(which npm)\nnpm_version: $(npm -v)\n"
+if which node | grep .nodebrew >/dev/null; then 
+    echo "already installed"
+else
+    mkdir -p ~/.nodebrew/src
+    nodebrew install stable
+    nodebrew use stable
+fi
+echo "
+node_path: $(which node)
+node_version: $(node -v)
+
+npm_path: $(which npm)
+npm_version: $(npm -v)
+"
+# echo "npm_path: $(which npm)\nnpm_version: $(npm -v)\n"
 
 # go
 echo "===== install go via goenv ====="
-goenv install 1.11.2
-goenv global 1.11.2
-goenv rehash
-echo "\npath: $(which go)\nversion: $(go version)"
+if which go | grep .goenv >/dev/null; then 
+    echo "already installed"
+else
+    goenv install 1.11.2
+    goenv global 1.11.2
+    goenv rehash
+fi
+echo "
+path: $(which go)
+version: $(go version)
+"
 
-echo "\nInstalled successfully"
+echo "Installed successfully"
