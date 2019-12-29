bbf75804ad4637632ca4a9b1c2545b0682559f82
yukin01 <bump1105@gmail.com>
2019-12-30 02:11:10 +0900

    Update install script for ansible

--- ansible/install.sh
+++ ansible/install.sh
@@ -4,7 +4,6 @@ set -eu
 printf "password: "
 read -s password
 
-echo $password | sudo -S apt update
-echo $password | sudo -S apt install --yes software-properties-common
-echo $password | sudo -S apt-add-repository --yes --update ppa:ansible/ansible
-echo $passowrd | sudo -S apt install --yes ansible
+echo $password | sudo -S sudo apt-get update
+echo $password | sudo -S sudo apt-get -y install python3-pip
+echo $password | sudo -S sudo pip3 install ansible
